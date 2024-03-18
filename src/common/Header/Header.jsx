b352dab98500045f4d077f58893dc22fccd93d66
Dave86dev <davidochando1986@gmail.com>
2024-03-18 10:39:06 +0100

    feat: enrutado activo mediante botones custom, sin necesidad de usar la barra de navegación

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,11 +1,28 @@
-
-import './Header.css'
+import { Navigator } from "../Navigator/Navigator";
+import "./Header.css";
 
 export const Header = () => {
+  const token = false;
+  
+  const logOut = () => {
+    //esta funcion me deslogeará en un breve futuro...
 
-    return (
-        <div className='headerDesign'>
+  }
 
+  return (
+    <div className="headerDesign">
+      <Navigator title={"home"} destination={"/"} />
+
+      {token ? (
+        <div>
+          <Navigator title={"nickdelusuario"} destination={"/"} />
+          <Navigator title={"log out"} onClick={() => logOut()} />
         </div>
-    )
-}
+      ) : (
+        <div>
+          <Navigator title={"register"} destination={"/register"} />
+        </div>
+      )}
+    </div>
+  );
+};

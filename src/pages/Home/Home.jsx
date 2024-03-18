9b7e360a6ef3ca6bf2e387f50111fcecc184bc03
Dave86dev <davidochando1986@gmail.com>
2024-03-18 10:18:30 +0100

    feat: Establecimiento básico de react-router dom, body de enrutado creado

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+
+export const Home = () => {
+
+    return (
+        <div className='homeDesign'>
+            soy la vista Home
+        </div>
+    )
+}
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,9 @@
+
+export const Register = () => {
+
+    return(
+        <div className='registerDesign'>
+            soy la vista register
+        </div>
+    )
+}
